e1e53e8541e84616bf277a0d8dd7cd090ace4275
8bitkick <8bitkick@users.noreply.github.com>
2019-03-07 13:51:05 -0800

    Simplified

--- examples/Bubble_Display_Counter/Bubble_Display_Counter.ino
+++ examples/Bubble_Display_Counter/Bubble_Display_Counter.ino
@@ -1,10 +1,10 @@
 /*
- Modified 2019 for 6 digit NSA1166 display
+Modified 2019 for 6 digit NSA1166 display
 
- ------------------------------------------
- Based on
- https://github.com/sparkfun/SevSeg
- http://arduino.cc/playground/Main/SevenSegmentLibrary
+------------------------------------------
+Based on
+https://github.com/sparkfun/SevSeg
+http://arduino.cc/playground/Main/SevenSegmentLibrary
 
 */
 
@@ -13,28 +13,13 @@
 //Create an instance of the object.
 SevSegPlus ledDisplay;
 
-//Create global variables
-unsigned long timer;
-int deciSecond = 0;
 
 void setup()
 {
   ledDisplay.begin();
-  timer = millis();
 }
 
 void loop()
 {
-
-  // Every 10ms we increase the number
-  if (millis() - timer >= 100)
-  {
-    timer = millis();
-    deciSecond++;
-
-    ledDisplay.print(deciSecond);
-  }
-
-  // Otherwise we can do something else
-  delay(5);
+  ledDisplay.print(millis());
 }
